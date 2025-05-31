b01e50c8566769ee080093702d15a0fce1716a83
Imadegbor Ehidiamen <imadehidiame@gmail.com>
2025-05-31 01:42:55 +0100

    update config.server.ts

--- app/config.server.ts
+++ app/config.server.ts
@@ -1,16 +1,16 @@
 export const sessionEnv = async ()=>{
     console.log('ENV FOR MG DB');
-    console.log(process.env.REACT_APP_MONGO_DB_CONNECTION_STRING)
+    console.log(process.env.MONGO_DB_CONNECTION_STRING)
     const Sessions = {
-        secret:process.env.REACT_APP_SESSION_SECRET,
-        name:process.env.REACT_APP_SESSION_NAME,
-        adm_secret:process.env.REACT_APP_ADM_SESSION_SECRET,
-        adm_name:process.env.REACT_APP_ADM_SESSION_NAME,
-        jwt_secret:process.env.REACT_APP_AUTH_KEY_NAME,
+        secret:process.env.SESSION_SECRET,
+        name:process.env.SESSION_NAME,
+        adm_secret:process.env.ADM_SESSION_SECRET,
+        adm_name:process.env.ADM_SESSION_NAME,
+        jwt_secret:process.env.AUTH_KEY_NAME,
         is_development:true 
     }; 
     const MongoDb = {
-        connection_url:process.env.REACT_APP_MONGO_DB_CONNECTION_STRING
+        connection_url:process.env.MONGO_DB_CONNECTION_STRING
     }
 
     return {Sessions,MongoDb}
